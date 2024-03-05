9cfbd4d8455c3db19831cbf2d253682590a0d23f
ayushofficial <ayush.singh@appventurez.com>
2024-03-05 13:32:45 +0530

    customize user details service

--- src/main/java/com/project/springSecurity/MySecurityConfig.java
+++ src/main/java/com/project/springSecurity/MySecurityConfig.java
@@ -4,14 +4,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class MySecurityConfig {
-	
+
+	@Bean
+	UserDetailsService UserdetailService() {
+		InMemoryUserDetailsManager userDetails = new InMemoryUserDetailsManager();
+		UserDetails user = User.withUsername("ayush").password(passwordEncode().encode("ayush.singh@security"))
+				.authorities("read").build();
+
+		userDetails.createUser(user);
+		return userDetails;
+	}
+
+	@Bean
+	BCryptPasswordEncoder passwordEncode() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		
+
 		http.httpBasic(Customizer.withDefaults());
 		http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
 		return http.build();
