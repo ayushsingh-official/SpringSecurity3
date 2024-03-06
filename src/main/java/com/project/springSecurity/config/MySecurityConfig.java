41022641accb204557b07bdb986837b3f65f888c
ayushofficial <ayush.singh@appventurez.com>
2024-03-06 13:46:44 +0530

    create custom filter

--- src/main/java/com/project/springSecurity/config/MySecurityConfig.java
+++ src/main/java/com/project/springSecurity/config/MySecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class MySecurityConfig {
@@ -25,15 +26,20 @@ public class MySecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 
+	/**
+	 * Authentication Filter
+	 */
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+//		http.formLogin(Customizer.withDefaults());
 		http.httpBasic(Customizer.withDefaults());
 //		http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
 //		http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/hello").authenticated());
 		http.authorizeHttpRequests(
 				authorize -> authorize.requestMatchers("/hello").authenticated().anyRequest().denyAll());
 
+		// do filter before BasicAuthenticationFilter class
+		http.addFilterBefore(new MyCustomFilter(), BasicAuthenticationFilter.class);
 		return http.build();
 	}
 }
