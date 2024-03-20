da59bb8a6a202b9e125098bfbc0baf19eeab32bf
ayush singh <ayush.singh@appventurez.com>
2024-03-20 13:26:41 +0530

    jwt claims

--- src/main/java/com/project/springSecurity/services/impl/JWTServiceImpl.java
+++ src/main/java/com/project/springSecurity/services/impl/JWTServiceImpl.java
@@ -2,10 +2,12 @@ package com.project.springSecurity.services.impl;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -19,12 +21,24 @@ public class JWTServiceImpl {
 		return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date(System.currentTimeMillis()))
 				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
 				.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
-
 	}
 
 	private Key getSignKey() {
 		byte[] key = Decoders.BASE64.decode("413F4428472B4B6250655368566D5970337336763979244226452948404D6351");
 		return Keys.hmacShaKeyFor(key);
+	}
+
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	private Claims extractAllClaims(String token) {
+		return Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token).getBody();
 	}
 
 }
